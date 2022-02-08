52fa1b27e098bf2cb084f5790505f3d4fc8e1887
24abhishek-baghel <abhishek.baghel@byjus.com>
2022-02-08 08:59:46 +0530

    Understanding CSS,Xpath, Assertion and wait mechanism

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -2,9 +2,26 @@ describe('Ecommerce Application', function()
 {
    it('Login page', function(){
        // webdriverIo code
-       browser.url('https://www.flipkart.com/')
-       console.log(browser.getTitle())
+       browser.url('https://rahulshettyacademy.com/loginpagePractise/#')
+       console.log(browser.getTitle())      
+       expect(browser).toHaveTitleContaining("Rahul Shetty Academy") // Assertion to validate title of webpage
+     
+       // Locators: CSS:
+       $("input[name='username']").setValue("rahulshettyacademy") // enetr value in edit box
        browser.pause(3000)
-   })
+
+       //XPath: //tagname[@attribute='value']
+      const password = $("//input[@name ='password']")
+      password.setValue("learning123")
+      $("#signInBtn").click()   // clicking on button
+
+      // $("#signInBtn").getAttribute('value') === "sign In"  --> Wait mechanism 
+      browser.waitUntil(()=> $("#signInBtn").getAttribute('value') === "sign In" , 
+          { timeout : 4000, timeoutMsg : "Error message " })
+
+     console.log($(".alert-danger").getText + "is the text i can see the screen") // extract the text out
+
+
+    })
 
 });
